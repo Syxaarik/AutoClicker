4e06de2b20c296a85f549cc68a55dedf864800ec
Artem <artemmarkovskiy2009@gmail.com>
2025-05-19 13:13:02 +0300

    Доработка интерфейса и получение кпс

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 from tkinter import *
 from tkinter import ttk
+import pyautogui
 from async_tkinter_loop import async_mainloop
-import asyncio
 
+pyautogui.FAILSAFE = True
+
+# Создание окна
 root = Tk()
 root.title("AutoClick")
-root.geometry("450x400")
+root.geometry('350x250')
 
 # Атрибуты окна
 root.resizable(False, False)
 root.attributes("-alpha", 0.7)
+root.attributes('-topmost', True)
+
+
+# AutoClicker
+def hello():
+    label = ttk.Label(text=f'Выбрано:{input_click.get()} cps')
+    label.place(x=120, y=20)
+
 
 # Кнопки
-btn = ttk.Button(text="Button")
-btn.pack()
+btn1 = ttk.Button(text='Start', command=hello)
+btn1.place(x=65, y=170, height=45, width=90)
+btn2 = ttk.Button(text='Stop', command=hello)
+btn2.place(x=195, y=170, height=45, width=90)
 
+input_click = ttk.Entry()
+input_click.place(x=95, y=45, height=20, width=160)
 
 async_mainloop(root)
