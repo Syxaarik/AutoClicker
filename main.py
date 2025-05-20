8f5d8a0a71aa77655a139e7041208277d9671b4d
Artem <artemmarkovskiy2009@gmail.com>
2025-05-20 09:24:37 +0300

    создание автоклика

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pyautogui
 from async_tkinter_loop import async_mainloop
 
 pyautogui.FAILSAFE = True
+click = True
 
 # Создание окна
 root = Tk()
@@ -17,18 +18,36 @@ root.attributes('-topmost', True)
 
 
 # AutoClicker
-def hello():
-    label = ttk.Label(text=f'Выбрано:{input_click.get()} cps')
-    label.place(x=120, y=20)
+def start_click():
+    global click
+    click_timer = input_click.get()
+    click = True
+
+    if click:
+        while click:
+            pyautogui.click()
+            root.update()
+    else:
+        click = True
+    root.update()
+
+
+def stop_click():
+    global click
+    click = False
+    root.update()
 
 
 # Кнопки
-btn1 = ttk.Button(text='Start', command=hello)
-btn1.place(x=65, y=170, height=45, width=90)
-btn2 = ttk.Button(text='Stop', command=hello)
-btn2.place(x=195, y=170, height=45, width=90)
+btn1 = ttk.Button(text='Start', command=start_click)
+btn1.place(x=195, y=170, height=45, width=90)
+btn2 = ttk.Button(text='Stop', command=stop_click)
+btn2.place(x=65, y=170, height=45, width=90)
 
 input_click = ttk.Entry()
 input_click.place(x=95, y=45, height=20, width=160)
+
+label = ttk.Label()
+label.place(x=120, y=20)
 
 async_mainloop(root)
