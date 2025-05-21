a70373741de5b65c0e325ab036970a811d4d7e9a
Artem <artemmarkovskiy2009@gmail.com>
2025-05-21 10:32:00 +0300

    доработка кнопок

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ pyautogui.FAILSAFE = True
 
 # Создание окна
 root = Tk()
-root.title("AutoClick")
+root.title('AutoClick')
 root.geometry('350x250')
 root.resizable(False, False)
-root.attributes("-alpha", 0.7)
+root.attributes('-alpha', 0.7)
 root.attributes('-topmost', True)
 
 # Переменные
@@ -40,20 +40,25 @@ def stop_click():
     click_active = False
 
 
-keyboard.add_hotkey('ctrl + 1', start_click)
-keyboard.add_hotkey('ctrl + 2', stop_click)
+# Назначение горячих клавиш
+keyboard.add_hotkey('f5', start_click)
+keyboard.add_hotkey('f6', stop_click)
 
-# Интерфейс (без изменений)
+# Интерфейс
 input_click = ttk.Entry()
 input_click.place(x=95, y=45, height=20, width=160)
+input_click.insert(0, "10")  # Значение по умолчанию
 
-label = ttk.Label(text="Задержка (мс):")
-label.place(x=120, y=20)
+ttk.Label(text='Задержка (мс):').place(x=120, y=20)
 
+# Надписи над кнопками
+ttk.Label(text="Start (F5)").place(x=211, y=150)
+ttk.Label(text="Stop (F6)").place(x=81, y=150)
+
+# Кнопки
 btn1 = ttk.Button(text='Start', command=start_click)
 btn1.place(x=195, y=170, height=45, width=90)
 btn2 = ttk.Button(text='Stop', command=stop_click)
 btn2.place(x=65, y=170, height=45, width=90)
 
-# Запуск главного цикла (обязательно!)
 root.mainloop()
